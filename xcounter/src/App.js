47f23fc65949292db8e19689f22463106480e968
Prabukhanth T <prabu@criodo.com>
2024-10-10 22:37:07 +0530

    xcounter

--- xcounter/src/App.js
+++ xcounter/src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useReducer, useState } from 'react'
+
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement'
+}
+
+function reducer(state, action){
+  switch (action.type){
+    case ACTIONS.INCREMENT:
+      return {count : state.count + 1}
+    case ACTIONS.DECREMENT:
+      return {count : state.count - 1}
+    default:
+      return state
+  }      
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, {count : 0})
+
+  const handleInc = () => {
+    dispatch({type: ACTIONS.INCREMENT});
+  }
+
+  const handleDec = () => {
+    dispatch({ type: ACTIONS.DECREMENT });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>
+          Counter App
+        </h1>
+      </div>
+      <div>
+        <p>Count: {state.count}</p>
+        <button onClick={handleInc}>Increment</button>
+        <button onClick={handleDec}>Decrement</button>
+      </div>
     </div>
   );
 }
